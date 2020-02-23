a33ba71b78649dde41a3b8e3e1846f9bb54ce34d
Mai Aloni <maiaa1993@gmail.com>
2020-02-23 12:08:35 +0200

    added delete functionality 12:08

--- src/components/TodoPreview.js
+++ src/components/TodoPreview.js
@@ -3,19 +3,21 @@ import { Link } from 'react-router-dom';
 
 export default function TodoPreview(props) {
     return (
-        // <Link to={`/todos/${props.todo.id}`}>
+        <Link to={`/todos/${props.todo.id}`}>
             <li className="todo-preview">
                 <div className="todo-line">
                     <div className="todo-title-preview">⭕ {props.todo.title}</div>
                     <Link to={`/todos/${props.todo.id}/edit`}>
-                    <button className="edit-button"><img src="https://res.cloudinary.com/dtwqtpteb/image/upload/v1582406346/zwvointr6yjve95cf4bw.png"
-                        width="23" height="23" alt="trash" /></button>
-                     </Link>
+                        <button className="edit-button"><img src="https://res.cloudinary.com/dtwqtpteb/image/upload/v1582406346/zwvointr6yjve95cf4bw.png"
+                            width="23" height="23" alt="trash" /></button>
+                    </Link>
                     <button className="trash-button"><img src="https://res.cloudinary.com/dtwqtpteb/image/upload/v1582404089/qqj9fcfqjjtphfiye2u2.png"
-                        width="23" height="23" alt="trash"/></button>
+                        width="23" height="23" alt="trash"
+                        onClick={() => { props.onDelete(props.todo) }} />
+                    </button>
                 </div>
             </li>
-        // </Link>
+        </Link>
     )
 }
 
