f392a6a223e73761c05cca803df453b05cba644d
Mai Aloni <maiaa1993@gmail.com>
2020-02-23 15:31:50 +0200

    Updates

---
+++ precache-manifest.6597b28b639362dc2c383939f290b8b0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cb88a545fb48f928ea0ec07296a71bc",
+    "url": "/todo_list/index.html"
+  },
+  {
+    "revision": "d86716ce071d4954179c",
+    "url": "/todo_list/static/css/main.47728778.chunk.css"
+  },
+  {
+    "revision": "c09d34544de9d787f47d",
+    "url": "/todo_list/static/js/2.4cb62e4c.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/todo_list/static/js/2.4cb62e4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d86716ce071d4954179c",
+    "url": "/todo_list/static/js/main.4b42be00.chunk.js"
+  },
+  {
+    "revision": "3f57be554bc8bef09089",
+    "url": "/todo_list/static/js/runtime-main.cbeaa8c1.js"
+  },
+  {
+    "revision": "44dfe8cc676882243911a3197a50169e",
+    "url": "/todo_list/static/media/Lato-Bold.44dfe8cc.ttf"
+  },
+  {
+    "revision": "7f690e503a254e0b8349aec0177e07aa",
+    "url": "/todo_list/static/media/Lato-Regular.7f690e50.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/todo_list/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c641dbee1d75892e4d88bdc31560c91b",
+    "url": "/todo_list/static/media/Montserrat-SemiBold.c641dbee.ttf"
+  }
+]);
